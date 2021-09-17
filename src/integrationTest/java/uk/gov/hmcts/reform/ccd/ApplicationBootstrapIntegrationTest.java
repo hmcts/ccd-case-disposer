2b4462ae215c500f397d10defbe93b5c2521d919
Semenu Aggor <semenu.aggor@HMCTS.NET>
2021-09-17 19:01:12 +0100

    RDM-12809 Configure into a non-web application

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/ccd/ApplicationBootstrapIntegrationTest.java
@@ -0,0 +1,13 @@
+package uk.gov.hmcts.reform.ccd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationBootstrapIntegrationTest {
+
+    @Test
+    void testShouldBootstrapSpringContext() {
+    }
+
+}
---
+++ src/main/java/uk/gov/hmcts/reform/ccd/ApplicationBootstrap.java
@@ -0,0 +1,32 @@
+package uk.gov.hmcts.reform.ccd;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import javax.inject.Inject;
+
+@Slf4j
+@SpringBootApplication
+@SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
+public class ApplicationBootstrap implements ApplicationRunner {
+
+    @Inject
+    private ApplicationExecutor applicationExecutor;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.info("Starting the Case-Disposer job.");
+        applicationExecutor.execute();
+        log.info("Completed the Case-Disposer job successfully.");
+    }
+
+    public static void main(final String[] args) {
+        final ApplicationContext context = SpringApplication.run(ApplicationBootstrap.class);
+        SpringApplication.exit(context);
+    }
+
+}
