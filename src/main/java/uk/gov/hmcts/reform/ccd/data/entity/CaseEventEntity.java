5d904aeafc6e8019150e2946e49cd2c712819105
KleoG <kleogeorgiou@hotmail.com>
2021-11-11 16:21:50 +0000

    RDM-12935 - revert Entity code

--- src/main/java/uk/gov/hmcts/reform/ccd/data/entity/CaseEventEntity.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/data/entity/CaseEventEntity.java
@@ -6,8 +6,6 @@ import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -19,7 +17,6 @@ import javax.persistence.Table;
 public class CaseEventEntity {
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "event_id")
     private String eventId;
