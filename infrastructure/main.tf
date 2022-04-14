2916c5754055cd4cd708de68e2fc3c8d8efb0ae8
Dave Jones <dave.jones@hmcts.net>
2022-04-14 15:02:31 +0100

    LAU-325: Add preview DB.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,105 @@
 provider "azurerm" {
   features {}
 }
+
+locals {
+  // Staging DB used as a data-store-api DB for functional testts in preview.
+  app_full_name = "${var.product}-data-store-api-staging"
+
+  // AAT Only
+  instance_count = (var.env == "aat") ? 1 : 0
+
+  // Vault name
+  vaultName = "${var.raw_product}-${var.env}"
+
+  // Shared Resource Group
+  sharedResourceGroup = "${var.raw_product}-shared-${var.env}"
+
+  sharedASPResourceGroup = "${var.raw_product}-shared-${var.env}"
+
+}
+
+data "azurerm_key_vault" "ccd_shared_key_vault" {
+  name                = "${local.vaultName}"
+  resource_group_name = "${local.sharedResourceGroup}"
+}
+
+resource "random_string" "draft_encryption_key" {
+  length  = 16
+  special = true
+  upper   = true
+  lower   = true
+  number  = true
+  lifecycle {
+    ignore_changes = all
+  }
+}
+
+////////////////////////////////
+// Populate Vault with DB info
+////////////////////////////////
+
+resource "azurerm_key_vault_secret" "POSTGRES-USER" {
+  name         = "${var.component}-POSTGRES-USER"
+  value        = module.data-store-preview-db-v11.user_name
+  key_vault_id = data.azurerm_key_vault.ccd_shared_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
+  name         = "${var.component}-POSTGRES-PASS"
+  value        = module.data-store-preview-db-v11.postgresql_password
+  key_vault_id = data.azurerm_key_vault.ccd_shared_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
+  name         = "${var.component}-POSTGRES-HOST"
+  value        = module.data-store-preview-db-v11.host_name
+  key_vault_id = data.azurerm_key_vault.ccd_shared_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
+  name         = "${var.component}-POSTGRES-PORT"
+  value        = module.data-store-preview-db-v11.postgresql_listen_port
+  key_vault_id = data.azurerm_key_vault.ccd_shared_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
+  name         = "${var.component}-POSTGRES-DATABASE"
+  value        = module.data-store-preview-db-v11.postgresql_database
+  key_vault_id = data.azurerm_key_vault.ccd_shared_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "ccd_draft_encryption_key" {
+  name         = "${var.component}-draftStoreEncryptionSecret"
+  value        = random_string.draft_encryption_key.result
+  key_vault_id = data.azurerm_key_vault.ccd_shared_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "draft-store-key" {
+  name         = "${var.component}-draft-key"
+  value        = random_string.draft_encryption_key.result
+  key_vault_id = data.azurerm_key_vault.ccd_shared_key_vault.id
+}
+
+////////////////////////////////
+// DB version 11              //
+////////////////////////////////
+
+module "data-store-preview-db-v11" {
+  count           = local.instance_count
+  source          = "git@github.com:hmcts/cnp-module-postgres?ref=master"
+  product         = var.product
+  component       = var.component
+  name            = "${local.app_full_name}-postgres-db-v11"
+  location        = "${var.location}"
+  env             = "${var.env}"
+  subscription    = "${var.subscription}"
+  postgresql_user = "${var.postgresql_user}"
+  database_name   = "${var.database_name}"
+  postgresql_version = "11"
+  sku_name        = "${var.database_sku_name}"
+  sku_tier        = "GeneralPurpose"
+  sku_capacity    = "${var.database_sku_capacity}"
+  storage_mb      = "${var.database_storage_mb}"
+  common_tags     = "${var.common_tags}"
+}
