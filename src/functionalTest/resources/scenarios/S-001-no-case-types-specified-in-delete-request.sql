a8e2b7843c5ea8d15e2ff3b13315bc1d1148686a
Dave Jones <dave.jones@hmcts.net>
2022-05-23 17:57:12 +0100

    LAU-325: Refact case types and update sql.

--- src/functionalTest/resources/scenarios/S-001-no-case-types-specified-in-delete-request.sql
+++ src/functionalTest/resources/scenarios/S-001-no-case-types-specified-in-delete-request.sql
@@ -1,7 +1,7 @@
-DELETE FROM case_link where case_id in (SELECT id FROM case_data where case_type_id in ('DPR_FT_MasterCaseType'));
-DELETE FROM case_link where linked_case_id in (SELECT id FROM case_data where case_type_id in ('DPR_FT_MasterCaseType'));
-DELETE FROM case_event where case_data_id in (SELECT id FROM case_data where case_type_id in ('DPR_FT_MasterCaseType'));
-DELETE FROM case_data where case_type_id in ('DPR_FT_MasterCaseType');
+DELETE FROM case_link where case_id in (SELECT id FROM case_data where case_type_id in ('DPR_FT_MasterCaseType','DPR_FT_MultiplePages','DPR_FT_Conditionals'));
+DELETE FROM case_link where linked_case_id in (SELECT id FROM case_data where case_type_id in ('DPR_FT_MasterCaseType','DPR_FT_MultiplePages','DPR_FT_Conditionals'));
+DELETE FROM case_event where case_data_id in (SELECT id FROM case_data where case_type_id in ('DPR_FT_MasterCaseType','DPR_FT_MultiplePages','DPR_FT_Conditionals'));
+DELETE FROM case_data where case_type_id in ('DPR_FT_MasterCaseType','DPR_FT_MultiplePages','DPR_FT_Conditionals');
 
 INSERT INTO case_data (case_type_id, jurisdiction, state, security_classification, data, data_classification, reference, resolved_ttl)
 VALUES ('DPR_FT_MasterCaseType',
