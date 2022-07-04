c45dd2b4ddb7bc07d07badbef6cc6c5fe8629b5a
Martin <martin.spasov@hmcts.net>
2022-07-04 12:08:22 +0100

    Update definition file

--- bin/import-functional-test-definitions-pipeline.sh
+++ bin/import-functional-test-definitions-pipeline.sh
@@ -7,5 +7,5 @@ xlsToJsonFolder=${1}
 
 echo xlsToJsonFolder = $xlsToJsonFolder
 
-echo Importing DISPOSER_Master_Definition.xlsx ....................
+echo Importing DISPOSER_Master_Definition_new.xlsx ....................
 ${dir}/ccd-import-definition-pipeline.sh "${xlsToJsonFolder}/DISPOSER_Master_Definition.xlsx"
