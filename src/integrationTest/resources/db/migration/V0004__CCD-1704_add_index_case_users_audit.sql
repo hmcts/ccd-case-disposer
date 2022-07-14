1638f76f7f8646cf14b1d95f3ef1ee1f4db71619
Martin <martin.spasov@hmcts.net>
2022-07-14 13:58:34 +0100

    LAU-442 Checkout integration test migration scripts from github

--- src/integrationTest/resources/db/migration/V0004__CCD-1704_add_index_case_users_audit.sql
+++
@@ -1,1 +0,0 @@
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_case_users_audit_case_data_id_user_id_case_role ON case_users_audit USING btree (case_data_id, user_id, case_role);
--- src/integrationTest/resources/db/migration/V0006__RDM_12840_add_resolved_ttl_to_case_data.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE ONLY public.case_data
-    ADD resolved_ttl date;
