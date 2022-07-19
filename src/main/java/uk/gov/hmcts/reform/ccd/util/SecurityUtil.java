e01d5e7fda25306765f7bdc66734f30601f13d80
Martin <martin.spasov@hmcts.net>
2022-07-19 09:28:33 +0100

    LAU-458 Performance: Cache user credentials

--- src/main/java/uk/gov/hmcts/reform/ccd/util/SecurityUtil.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/util/SecurityUtil.java
@@ -2,17 +2,25 @@ package uk.gov.hmcts.reform.ccd.util;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.ccd.parameter.ParameterResolver;
 import uk.gov.hmcts.reform.idam.client.IdamClient;
 
+import static java.util.concurrent.TimeUnit.MINUTES;
+
 @Service
 @Slf4j
+@EnableScheduling
 public class SecurityUtil {
 
     private final AuthTokenGenerator authTokenGenerator;
     private final IdamClient idamClient;
+
+    private String serviceAuthToken;
+    private String idamClientToken;
 
     @Autowired
     private ParameterResolver parameterResolver;
@@ -24,12 +32,17 @@ public class SecurityUtil {
     }
 
     public String getServiceAuthorization() {
-        return authTokenGenerator.generate();
+        return serviceAuthToken;
     }
 
     public String getIdamClientToken() {
-        return idamClient.getAccessToken(parameterResolver.getIdamUsername(),
-                                         parameterResolver.getIdamPassword());
+        return idamClientToken;
     }
 
+    @Scheduled(fixedRate = 55, timeUnit = MINUTES)
+    private void generateTokens() {
+        serviceAuthToken = authTokenGenerator.generate();
+        idamClientToken = idamClient.getAccessToken(parameterResolver.getIdamUsername(),
+                parameterResolver.getIdamPassword());
+    }
 }
