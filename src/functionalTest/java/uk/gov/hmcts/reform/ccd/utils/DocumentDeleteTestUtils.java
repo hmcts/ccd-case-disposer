aa11542b1f74a92fafa6cf14652e3525352c613c
Martin <martin.spasov@hmcts.net>
2023-02-08 10:38:53 +0000

    Add awaitibility for document and role deletions functional tests.

--- src/functionalTest/java/uk/gov/hmcts/reform/ccd/utils/DocumentDeleteTestUtils.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/ccd/utils/DocumentDeleteTestUtils.java
@@ -16,6 +16,7 @@ import java.util.Map;
 import javax.inject.Inject;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.awaitility.Awaitility.with;
 import static org.springframework.http.MediaType.IMAGE_JPEG_VALUE;
 import static uk.gov.hmcts.reform.ccd.constants.TestConstants.DOCUMENT_PATH;
 import static uk.gov.hmcts.reform.ccd.constants.TestConstants.DOCUMENT_STORE_USER;
@@ -39,15 +40,16 @@ public class DocumentDeleteTestUtils {
     private FileUtils fileUtils;
 
     public void verifyDocumentStoreDeletion(final Map<Long, List<String>> deletableDocuments) {
-        deletableDocuments.entrySet().forEach(entry -> {
-            final CaseDocumentsDeletionResults caseDocumentsDeletionResults = documentDeletionRecordHolder
-                    .getCaseDocumentsDeletionResults(Long.toString(entry.getKey()));
+        deletableDocuments.entrySet().forEach(entry -> with().await()
+                .untilAsserted(() -> {
+                    final CaseDocumentsDeletionResults caseDocumentsDeletionResults = documentDeletionRecordHolder
+                            .getCaseDocumentsDeletionResults(Long.toString(entry.getKey()));
 
-            assertThat(caseDocumentsDeletionResults).isNotNull();
-            assertThat(caseDocumentsDeletionResults.getMarkedForDeletion()).isNotZero();
-            assertThat(caseDocumentsDeletionResults.getCaseDocumentsFound()).isNotZero();
-        });
+                    assertThat(caseDocumentsDeletionResults).isNotNull();
+                    assertThat(caseDocumentsDeletionResults.getMarkedForDeletion()).isNotZero();
+                    assertThat(caseDocumentsDeletionResults.getCaseDocumentsFound()).isNotZero();
 
+                }));
     }
 
     public void uploadDocument(final Map<Long, List<String>> deletableDocuments) {
