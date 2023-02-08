44a5cfadbaf248a1f7233f7b701d5a0cba609e2d
Martin <martin.spasov@hmcts.net>
2023-02-08 11:36:55 +0000

    Refactor ES assertions

--- src/functionalTest/java/uk/gov/hmcts/reform/ccd/CaseDeletionFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/ccd/CaseDeletionFunctionalTest.java
@@ -27,7 +27,7 @@ class CaseDeletionFunctionalTest extends TestDataProvider {
     static void setup() {
         Awaitility.setDefaultPollInterval(0, TimeUnit.MILLISECONDS);
         Awaitility.setDefaultPollDelay(Duration.FIVE_SECONDS);
-        Awaitility.setDefaultTimeout(30, TimeUnit.SECONDS);
+        Awaitility.setDefaultTimeout(50, TimeUnit.SECONDS);
     }
 
     @ParameterizedTest
