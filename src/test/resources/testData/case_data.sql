b332b8880cd9f03fe4da052a02c00c7a336b7a5b
priyankaVerma21 <priyanka.verma@HMCTS.NET>
2025-01-13 11:48:29 +0000

    LAU-1011 JPA Repository Test (#720)
    * LAU-1011 JPA Repository Test

---
+++ src/test/resources/testData/case_data.sql
@@ -0,0 +1,54 @@
+DELETE FROM case_event;
+DELETE FROM case_link;
+DELETE FROM case_data;
+
+INSERT INTO case_data (id, case_type_id, jurisdiction, state, security_classification, data, data_classification, reference, resolved_ttl)
+VALUES (1, 'deletable_case_type', 'deletable_jurisdiction', 'CaseCreated', 'PUBLIC',
+        '{
+          "PersonFirstName": "Janet"
+        }',
+       '{
+         "PersonFirstName": "PUBLIC"
+       }',
+       1,
+       '2016-06-24 20:44:52.824'
+);
+
+INSERT INTO case_data (id, case_type_id, jurisdiction, state, security_classification, data, data_classification, reference, resolved_ttl)
+VALUES (2, 'deletable_case_type', 'deletable_jurisdiction', 'CaseCreated', 'PUBLIC',
+        '{
+          "PersonFirstName": "Janet"
+        }',
+       '{
+         "PersonFirstName": "PUBLIC"
+       }',
+       2,
+       '2016-06-24 20:44:52.824'
+);
+
+INSERT INTO case_data (id, case_type_id, jurisdiction, state, security_classification, data, data_classification, reference, resolved_ttl)
+VALUES (3, 'deletable_case_type', 'deletable_jurisdiction', 'CaseCreated', 'PUBLIC',
+        '{
+          "PersonFirstName": "Janet"
+        }',
+       '{
+         "PersonFirstName": "PUBLIC"
+       }',
+       3,
+       NOW()
+);
+
+INSERT INTO case_data (id, case_type_id, jurisdiction, state, security_classification, data, data_classification, reference, resolved_ttl)
+VALUES (4, 'deletable_case_type', 'deletable_jurisdiction', 'CaseCreated', 'PUBLIC',
+        '{
+          "PersonFirstName": "Janet"
+        }',
+       '{
+         "PersonFirstName": "PUBLIC"
+       }',
+       4,
+       NOW()
+);
+
+
+
