29f1582e519bf929ae3477ef1d55dc27ad42e5cf
Rapolas <130578328+rapolaskaseliscgi@users.noreply.github.com>
2025-01-31 09:26:06 +0000

    LAU-1052 check if case links were deleted before proceeding to case deletion

--- src/test/java/uk/gov/hmcts/reform/ccd/util/ProcessedCasesRecordHolderTest.java
+++ src/test/java/uk/gov/hmcts/reform/ccd/util/ProcessedCasesRecordHolderTest.java
@@ -57,6 +57,27 @@ class ProcessedCasesRecordHolderTest {
         // then
         assertThat(processedCasesRecordHolder.getSuccessfullyDeletedCases()).containsOnly(caseDataProcessed1);
         assertThat(processedCasesRecordHolder.getFailedToDeleteDeletedCases()).containsOnly(caseDataFailed3);
+    }
 
+    @Test
+    void shouldClearState() {
+        // given
+        CaseData caseDataProcessed1 = CaseData.builder().id(1L).reference(1L).build();
+        CaseData caseDataProcessed2 = CaseData.builder().id(2L).reference(2L).build();
+        CaseData caseDataFailed3 = CaseData.builder().id(3L).reference(3L).build();
+        CaseData caseDataFailed4 = CaseData.builder().id(4L).reference(4L).build();
+
+        // when
+        processedCasesRecordHolder.addProcessedCase(caseDataProcessed1);
+        processedCasesRecordHolder.addProcessedCase(caseDataProcessed2);
+        processedCasesRecordHolder.addProcessedCase(caseDataFailed3);
+        processedCasesRecordHolder.addProcessedCase(caseDataFailed4);
+        processedCasesRecordHolder.addFailedToDeleteCaseRef(caseDataFailed3);
+        processedCasesRecordHolder.addFailedToDeleteCaseRef(caseDataFailed4);
+        processedCasesRecordHolder.clearState();
+
+        // then
+        assertThat(processedCasesRecordHolder.getSuccessfullyDeletedCases()).isEmpty();
+        assertThat(processedCasesRecordHolder.getFailedToDeleteDeletedCases()).isEmpty();
     }
 }
