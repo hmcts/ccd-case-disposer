3ef730a4d6e9858bae8ef9489bab08866113a1bd
Rapolas <130578328+rapolaskaseliscgi@users.noreply.github.com>
2025-12-18 17:00:28 +0000

    LAU-1315 make deletion multithreaded

--- src/main/java/uk/gov/hmcts/reform/ccd/util/log/HearingDeletionRecordHolder.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/util/log/HearingDeletionRecordHolder.java
@@ -3,31 +3,19 @@ package uk.gov.hmcts.reform.ccd.util.log;
 import jakarta.inject.Named;
 import lombok.Getter;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 @Named
 @Getter
 public class HearingDeletionRecordHolder {
-    private List<Map<String, Integer>> hearingDeletionRecordHolderList = new ArrayList<>();
+    private ConcurrentMap<String, Integer> hearingDeletionByCaseRef = new ConcurrentHashMap<>();
 
-    public void setHearingDeletionResults(final String caseRef,
-                                          final int hearingDeletionStatus) {
-        hearingDeletionRecordHolderList.add(Map.of(caseRef, hearingDeletionStatus));
+    public void setHearingDeletionResults(final String caseRef, final int deletionStatus) {
+        hearingDeletionByCaseRef.put(caseRef, deletionStatus);
     }
 
     public int getHearingDeletionResults(final String caseRef) {
-        if (!hearingDeletionRecordHolderList.isEmpty()) {
-            final Optional<Map<String, Integer>> deletionResultsMap =
-                hearingDeletionRecordHolderList.stream()
-                    .filter(hearingHolderEntry -> hearingHolderEntry.containsKey(caseRef))
-                    .findFirst();
-            if (deletionResultsMap.isPresent()) {
-                return deletionResultsMap.get().get(caseRef);
-            }
-        }
-        return 0;
+        return hearingDeletionByCaseRef.getOrDefault(caseRef, 0);
     }
 }
--- src/main/java/uk/gov/hmcts/reform/ccd/util/log/TasksDeletionRecordHolder.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/util/log/TasksDeletionRecordHolder.java
@@ -3,31 +3,20 @@ package uk.gov.hmcts.reform.ccd.util.log;
 import jakarta.inject.Named;
 import lombok.Getter;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 @Named
 @Getter
 public class TasksDeletionRecordHolder {
-    private List<Map<String, Integer>> tasksDeletionRecordHolderList = new ArrayList<>();
 
-    public void setCaseTasksDeletionResults(final String caseRef,
-                                            final int caseTasksDeletionResults) {
-        tasksDeletionRecordHolderList.add(Map.of(caseRef, caseTasksDeletionResults));
+    private ConcurrentMap<String, Integer> tasksDeletionByCaseRef = new ConcurrentHashMap<>();
+
+    public void setCaseTasksDeletionResults(final String caseRef, final int deletionResult) {
+        tasksDeletionByCaseRef.put(caseRef, deletionResult);
     }
 
     public int getTasksDeletionResults(final String caseRef) {
-        if (!tasksDeletionRecordHolderList.isEmpty()) {
-            final Optional<Map<String, Integer>> deletionResultsMap =
-                tasksDeletionRecordHolderList.stream()
-                    .filter(taskHolderEntry -> taskHolderEntry.containsKey(caseRef))
-                    .findFirst();
-            if (deletionResultsMap.isPresent()) {
-                return deletionResultsMap.get().get(caseRef);
-            }
-        }
-        return 0;
+        return tasksDeletionByCaseRef.getOrDefault(caseRef, 0);
     }
 }
