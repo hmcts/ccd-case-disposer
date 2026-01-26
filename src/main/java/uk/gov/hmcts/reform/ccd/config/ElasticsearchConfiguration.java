d6e64cc46c4677a6bf7fcf0785649538764ca82f
Priyanka Verma <priyanka.verma@hmcts.net>
2026-01-26 15:33:33 +0000

    LAU-1352 elastic search upgrade to 9.2.4 with Rest5Client

--- src/main/java/uk/gov/hmcts/reform/ccd/config/ElasticsearchConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/config/ElasticsearchConfiguration.java
@@ -3,15 +3,17 @@ package uk.gov.hmcts.reform.ccd.config;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
-import co.elastic.clients.transport.rest_client.RestClientTransport;
+import co.elastic.clients.transport.rest5_client.Rest5ClientTransport;
+import co.elastic.clients.transport.rest5_client.low_level.Rest5Client;
 import jakarta.annotation.PostConstruct;
 import jakarta.inject.Inject;
-import org.apache.http.HttpHost;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder;
+import org.apache.hc.core5.http.HttpHost;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.gov.hmcts.reform.ccd.parameter.ParameterResolver;
+
+import java.net.URISyntaxException;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class ElasticsearchConfiguration {
@@ -27,14 +29,21 @@ public class ElasticsearchConfiguration {
     @PostConstruct
     public void init() {
         final HttpHost[] httpHosts = parameterResolver.getElasticsearchHosts().stream()
-            .map(HttpHost::create)
+            .map(host -> {
+                try {
+                    return HttpHost.create(host);
+                } catch (URISyntaxException e) {
+                    throw new IllegalArgumentException("Invalid Elasticsearch host: " + host, e);
+                }
+            })
             .toArray(HttpHost[]::new);
 
-        RestClientBuilder builder = RestClient.builder(httpHosts)
+        Rest5Client restClient = Rest5Client.builder(httpHosts)
             .setRequestConfigCallback(requestConfigBuilder -> requestConfigBuilder
-                .setConnectTimeout(parameterResolver.getElasticsearchRequestTimeout())
-            );
-        ElasticsearchTransport transport = new RestClientTransport(builder.build(), new JacksonJsonpMapper());
+                .setConnectTimeout(parameterResolver.getElasticsearchRequestTimeout(), TimeUnit.MILLISECONDS)
+            )
+            .build();
+        ElasticsearchTransport transport = new Rest5ClientTransport(restClient, new JacksonJsonpMapper());
         elasticsearchClient = new ElasticsearchClient(transport);
     }
 
