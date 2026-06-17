dfc8bd694012bf29385779529146550e9dc8d887
Rapolas <130578328+rapolaskaseliscgi@users.noreply.github.com>
2026-06-17 18:20:34 +0100

    LAU-1477 update idam auth url

--- bin/load-aat-environment-variables.sh
+++ bin/load-aat-environment-variables.sh
@@ -6,6 +6,6 @@ echo 'export ENVIRONMENT=aat'
 
 # urls
 echo "export IDAM_S2S_URL=http://rpe-service-auth-provider-aat.service.core-compute-aat.internal"
-echo "export IDAM_API_URL=https://idam-api.aat.platform.hmcts.net"
+echo "export IDAM_API_URL=https://idam-web-public.aat.platform.hmcts.net"
 echo "export CCD_IDAM_REDIRECT_URL=https://ccd-case-management-web-aat.service.core-compute-aat.internal/oauth2redirect"
 echo "export DEFINITION_STORE_HOST=http://ccd-definition-store-api-aat.service.core-compute-aat.internal"
