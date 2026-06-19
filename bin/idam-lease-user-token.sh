38cbd1890cb3da0c41069368e69e1ac505a3b061
Rapolas <130578328+rapolaskaseliscgi@users.noreply.github.com>
2026-06-19 11:47:32 +0100

    Update token extract for FTs

--- bin/idam-lease-user-token.sh
+++ bin/idam-lease-user-token.sh
@@ -7,8 +7,15 @@ set -eu
 username=${1}
 password=${2}
 clientSecret=${API_GATEWAY_IDAM_SECRET:-ccd_gateway_secret}
-redirectUri=${CCD_IDAM_REDIRECT_URL:-http://localhost:3451/oauth2redirect}
 
-code=$(curl --insecure --fail --show-error --silent -X POST --user "${username}:${password}" "${IDAM_API_URL:-http://localhost:5000}/oauth2/authorize?redirect_uri=${redirectUri}&response_type=code&client_id=ccd_gateway" -d "" | docker run --rm --interactive hmctsprod.azurecr.io/imported/mikefarah/yq -r .code)
-
-curl --insecure --fail --show-error --silent -X POST -H "Content-Type: application/x-www-form-urlencoded" --user "ccd_gateway:${clientSecret}" "${IDAM_API_URL:-http://localhost:5000}/oauth2/token?code=${code}&redirect_uri=${redirectUri}&grant_type=authorization_code" -d "" | docker run --rm --interactive hmctsprod.azurecr.io/imported/mikefarah/yq -r .access_token
+curl --insecure --fail --show-error --silent \
+  -X POST \
+  -H "Content-Type: application/x-www-form-urlencoded" \
+  "${IDAM_API_URL:-http://localhost:5000}/o/token" \
+  --data-urlencode "grant_type=password" \
+  --data-urlencode "username=${username}" \
+  --data-urlencode "password=${password}" \
+  --data-urlencode "client_id=ccd_gateway" \
+  --data-urlencode "client_secret=${clientSecret}" \
+  --data-urlencode "scope=openid profile roles" \
+  | docker run --rm --interactive hmctsprod.azurecr.io/imported/mikefarah/yq -r .access_token
