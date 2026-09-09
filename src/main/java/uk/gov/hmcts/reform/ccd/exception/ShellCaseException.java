6a2124243da43606b704adc9b4e56e0a15fa2baf
Rapolas <130578328+rapolaskaseliscgi@users.noreply.github.com>
2026-09-09 11:43:45 +0100

    LAU-1501 copy json data based on shell mappings

--- src/main/java/uk/gov/hmcts/reform/ccd/exception/ShellCaseException.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/exception/ShellCaseException.java
@@ -1,4 +1,16 @@
 package uk.gov.hmcts.reform.ccd.exception;
 
 public class ShellCaseException extends RuntimeException {
+
+    public ShellCaseException() {
+        super();
+    }
+
+    public ShellCaseException(String message) {
+        super(message);
+    }
+
+    public ShellCaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
