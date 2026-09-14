17b6edaadaf9ffd166f5ee26c7b9c87681c34cc7
Rapolas <130578328+rapolaskaseliscgi@users.noreply.github.com>
2026-09-14 14:14:57 +0100

    LAU-1503 submit case

--- src/main/java/uk/gov/hmcts/reform/ccd/shell/service/client/CcdClient.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/shell/service/client/CcdClient.java
@@ -3,14 +3,17 @@ package uk.gov.hmcts.reform.ccd.shell.service.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import uk.gov.hmcts.reform.ccd.shell.data.CcdCaseResponse;
+import uk.gov.hmcts.reform.ccd.shell.data.CcdCreateCaseEventResponse;
+import uk.gov.hmcts.reform.ccd.shell.model.ShellCasePayload;
 
 import static uk.gov.hmcts.reform.ccd.util.RestConstants.AUTHORISATION_HEADER;
 import static uk.gov.hmcts.reform.ccd.util.RestConstants.SERVICE_AUTHORISATION_HEADER;
 
 @FeignClient(name = "ccd-client", url = "${remote.ccd-data-store.host}")
-@SuppressWarnings("PMD.ImplicitFunctionalInterface")
 public interface CcdClient {
 
     @GetMapping(value = "/cases/{caseReference}", headers = "Experimental=true")
@@ -19,4 +22,20 @@ public interface CcdClient {
         @RequestHeader(AUTHORISATION_HEADER) String authHeader,
         @PathVariable("caseReference") long caseReference
     );
+
+    @GetMapping(value = "/case-types/{caseType}/event-triggers/{triggerId}", headers = "Experimental=true")
+    CcdCreateCaseEventResponse getCreateCaseToken(
+        @RequestHeader(SERVICE_AUTHORISATION_HEADER) String serviceAuthHeader,
+        @RequestHeader(AUTHORISATION_HEADER) String authHeader,
+        @PathVariable("triggerId") String triggerId,
+        @PathVariable("caseType") String caseType
+    );
+
+    @PostMapping(value = "/case-types/{caseType}/cases", headers = "Experimental=true")
+    void createCase(
+        @RequestHeader(SERVICE_AUTHORISATION_HEADER) String serviceAuthHeader,
+        @RequestHeader(AUTHORISATION_HEADER) String authHeader,
+        @PathVariable("caseType") String caseType,
+        @RequestBody ShellCasePayload payload
+        );
 }
